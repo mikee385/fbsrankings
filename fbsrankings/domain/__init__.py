84dad14f75e32623a1a5c139697405158c253465
Michael Pierce <mikee385@gmail.com>
2019-01-05 14:47:43 -0600

    Create model and service folders in domain

--- fbsrankings/domain/__init__.py
+++ fbsrankings/domain/__init__.py
@@ -1,9 +1,9 @@
 """Domain classes and methods for the fbsrankings package"""
 
-from fbsrankings.domain.season import Season, SeasonID, SeasonFactory, SeasonRepository, SeasonSection
-from fbsrankings.domain.team import Team, TeamID, TeamFactory, TeamRepository
-from fbsrankings.domain.affiliation import Affiliation, AffiliationID, AffiliationFactory, AffiliationRepository, Subdivision
-from fbsrankings.domain.game import Game, GameID, GameFactory, GameRepository, GameStatus
-from fbsrankings.domain.factory import Factory
-from fbsrankings.domain.importservice import ImportService
-from fbsrankings.domain.cancelservice import CancelService
+from fbsrankings.domain.model.season import Season, SeasonID, SeasonFactory, SeasonRepository, SeasonSection
+from fbsrankings.domain.model.team import Team, TeamID, TeamFactory, TeamRepository
+from fbsrankings.domain.model.affiliation import Affiliation, AffiliationID, AffiliationFactory, AffiliationRepository, Subdivision
+from fbsrankings.domain.model.game import Game, GameID, GameFactory, GameRepository, GameStatus
+from fbsrankings.domain.model.factory import Factory
+from fbsrankings.domain.service.importservice import ImportService
+from fbsrankings.domain.service.cancelservice import CancelService
