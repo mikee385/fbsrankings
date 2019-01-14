8aa6e1f819e719ad0ad9e9b1611fd4499c633537
Michael Pierce <mikee385@gmail.com>
2019-01-13 20:16:56 -0600

    Store imported entities locally in ImportService

--- fbsrankings/domain/service/importservice.py
+++ fbsrankings/domain/service/importservice.py
@@ -23,28 +23,74 @@ class ImportService (object):
             raise TypeError('repository must be of type GameRepository')
         self._repository = repository
         
+        self._seasons = {}
+        self._teams = {}
+        self._affiliations = {}
+        self._games = {}
+        
+    def seasons(self):
+        return self._seasons.values()
+        
+    def teams(self):
+        return self._teams.values()
+        
+    def affiliations(self):
+        return self._affiliations.values()
+        
+    def games(self):
+        return self._games.values()
+        
     def import_season(self, year):
-        season = self._repository.find_season_by_year(year)
+        key = year
+        
+        season = self._seasons.get(key)
         if season is None:
-            season = self._factory.new_season(year)
-            self._repository.add_season(season)
+            season = self._repository.find_season_by_year(year)
+            if season is None:
+                season = self._factory.new_season(year)
+                self._repository.add_season(season)
+            self._seasons[key] = season
+            
         return season
         
     def import_team(self, name):
-        team = self._repository.find_team_by_name(name)
+        key = name
+        
+        team = self._teams.get(key)
         if team is None:
-            team = self._factory.new_team(name)
-            self._repository.add_team(team)
+            team = self._repository.find_team_by_name(name)
+            if team is None:
+                team = self._factory.new_team(name)
+                self._repository.add_team(team)
+            self._teams[key] = team
+            
         return team
         
     def import_affiliation(self, season_ID, team_ID, subdivision):
-        affiliation = self._repository.find_affiliation_by_season_team(season_ID, team_ID)
+        key = (season_ID, team_ID)
+        
+        affiliation = self._affiliations.get(key)
         if affiliation is None:
-            affiliation = self._factory.new_affiliation(season_ID, team_ID, subdivision)
-            self._repository.add_affiliation(affiliation)
+            affiliation = self._repository.find_affiliation_by_season_team(season_ID, team_ID)
+            if affiliation is None:
+                affiliation = self._factory.new_affiliation(season_ID, team_ID, subdivision)
+                self._repository.add_affiliation(affiliation)
+            self._affiliations[key] = affiliation
+            
         return affiliation
         
     def import_game(self, season_ID, week, date_, season_section, home_team_ID, away_team_ID, notes):
-        game = self._factory.new_game(season_ID, week, date_, season_section, home_team_ID, away_team_ID, notes)
-        self._repository.add_game(game)
+        if home_team_ID < away_team_ID:
+            key = (season_ID, season_section, week, home_team_ID, away_team_ID)
+        else:
+            key = (season_ID, season_section, week, away_team_ID, home_team_ID)
+            
+        game = self._games.get(key)
+        if game is None:
+            self._repository.find_game_by_season_teams(season_ID, season_section, week, home_team_ID, away_team_ID)
+            if game is None:
+                game = self._factory.new_game(season_ID, week, date_, season_section, home_team_ID, away_team_ID, notes)
+                self._repository.add_game(game)
+            self._games[key] = game
+            
         return game
