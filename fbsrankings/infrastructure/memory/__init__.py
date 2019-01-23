b8bfc98470136036a63b2e8167511b4001fefa3a
Michael Pierce <mikee385@gmail.com>
2019-01-22 20:16:20 -0600

    Remove 'repository' from infrastructure modules

--- fbsrankings/infrastructure/memory/__init__.py
+++ fbsrankings/infrastructure/memory/__init__.py
@@ -1,8 +1,8 @@
 """In-memory repositories for storing the domain classes of the fbsrankings package"""
 
-from fbsrankings.infrastructure.memory.seasonrepository import SeasonDataStore, SeasonRepository
-from fbsrankings.infrastructure.memory.teamrepository import TeamDataStore, TeamRepository
-from fbsrankings.infrastructure.memory.affiliationrepository import AffiliationDataStore, AffiliationRepository
-from fbsrankings.infrastructure.memory.gamerepository import GameDataStore, GameRepository
+from fbsrankings.infrastructure.memory.season import SeasonDataStore, SeasonRepository
+from fbsrankings.infrastructure.memory.team import TeamDataStore, TeamRepository
+from fbsrankings.infrastructure.memory.affiliation import AffiliationDataStore, AffiliationRepository
+from fbsrankings.infrastructure.memory.game import GameDataStore, GameRepository
 from fbsrankings.infrastructure.memory.repository import DataStore, Repository
 from fbsrankings.infrastructure.memory.unitofwork import UnitOfWork, UnitOfWorkFactory
