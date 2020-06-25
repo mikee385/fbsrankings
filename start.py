764d3ba75f968129bf5420e5e850b2337003a5b3
Michael Pierce <mikee385@gmail.com>
2020-06-25 14:45:54 -0500

    Add optional runtime type checking

--- start.py
+++ start.py
@@ -2,4 +2,8 @@ import os
 import sys
 sys.path.append(os.getcwd())
 
+#from typeguard.importhook import install_import_hook
+#with install_import_hook('fbsrankings'):
+#    import fbsrankings.main
+
 import fbsrankings.main
