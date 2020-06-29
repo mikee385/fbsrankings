0d7ee72943af913f5557274acbdb6dfb890a8b94
Michael Pierce <mikee385@gmail.com>
2020-06-29 08:27:26 -0500

    Minor change to paths in __main__

--- fbsrankings/__main__.py
+++ fbsrankings/__main__.py
@@ -6,9 +6,11 @@ import sys
 if sys.path[0] in ("", os.getcwd()):
     sys.path.pop(0)
 
-project_dir = os.path.dirname(os.path.dirname(__file__))
+package_dir = os.path.dirname(__file__)
+os.chdir(package_dir)
+
+project_dir = os.path.dirname(package_dir)
 sys.path.insert(0, project_dir)
-os.chdir(os.path.join(project_dir, "fbsrankings"))
 
 if "--use-typeguard" in sys.argv:
     print("Performing runtime type checks...")
