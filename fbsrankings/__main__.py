1a59035fa38884980f184421f4c91681e1d33f9d
Michael Pierce <mikee385@gmail.com>
2020-07-30 08:15:28 -0500

    Revert to setting up typeguard on import, instead of at runtime

--- fbsrankings/__main__.py
+++ fbsrankings/__main__.py
@@ -1,25 +1,19 @@
 import sys
-from warnings import filterwarnings
 
-from typeguard import TypeChecker, TypeWarning
+use_typeguard = False
+if "--use-typeguard" in sys.argv:
+    sys.argv.remove("--use-typeguard")
+    use_typeguard = True
 
-from fbsrankings import cli
+if use_typeguard:
+    print("Performing runtime type checks...")
 
+    from typeguard.importhook import install_import_hook
 
-def main():
-    use_typeguard = False
-    if "--use-typeguard" in sys.argv:
-        sys.argv.remove("--use-typeguard")
-        use_typeguard = True
-
-    if use_typeguard:
-        print("Performing runtime type checks...")
-
-        filterwarnings("always", category=TypeWarning)
-        with TypeChecker(["fbsrankings"]):
-            cli.main()
-    else:
-        cli.main()
+    with install_import_hook("fbsrankings"):
+        from fbsrankings.cli import main
+else:
+    from fbsrankings.cli import main
 
 if __name__ == "__main__":
     main()
