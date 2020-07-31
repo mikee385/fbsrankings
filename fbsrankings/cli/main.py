85c49b6593139475ae8a63d38ad949acc20374b0
Michael Pierce <mikee385@gmail.com>
2020-07-30 22:52:07 -0500

    Remove print command and add defaults

--- fbsrankings/cli/main.py
+++ fbsrankings/cli/main.py
@@ -47,7 +47,8 @@ import_parser.add_argument(
     "seasons",
     metavar="SEASON",
     type=SeasonRangeType(),
-    nargs="+",
+    nargs="*",
+    default=["latest"],
     action="store",
     help="Single season (e.g. 2018), range of seasons (e.g. 2015-2018), 'latest' to import the most recent season, or 'all' to import all available seasons. Multiple seasons and/or ranges can also be provided (e.g. 2010 2012-2014 2016 2018-2020).",
 )
@@ -77,33 +78,14 @@ def import_seasons(args: argparse.Namespace) -> None:
 
 import_parser.set_defaults(func=import_seasons)
 
-# PRINT--------------------------------------
+# LATEST-------------------------------------
 
-print_parser = subparsers.add_parser(
-    "print",
-    description="Print seasons, team rankings, or game rankings.",
-    parents=[common_parser],
-)
-
-
-def print_results(args: argparse.Namespace) -> None:
-    if hasattr(args, "print_func"):
-        args.print_func(args)
-    else:
-        print_parser.print_help()
-
-
-print_parser.set_defaults(func=print_results)
-print_subparsers = print_parser.add_subparsers()
-
-# PRINT LATEST-------------------------------
-
-print_latest_parser = print_subparsers.add_parser(
+latest_parser = subparsers.add_parser(
     "latest",
     description="Print team and game rankings for the most recent completed week.",
     parents=[common_parser],
 )
-print_latest_parser.add_argument(
+latest_parser.add_argument(
     "-r",
     "--rating",
     type=str.casefold,
@@ -112,7 +94,7 @@ print_latest_parser.add_argument(
     action="store",
     help="rating calculation to use for rankings",
 )
-print_latest_parser.add_argument(
+latest_parser.add_argument(
     "-t",
     "--top",
     metavar="COUNT",
@@ -133,16 +115,16 @@ def print_latest(args: argparse.Namespace) -> None:
             print_err(str(ex))
 
 
-print_latest_parser.set_defaults(print_func=print_latest)
+latest_parser.set_defaults(func=print_latest)
 
-# PRINT SEASONS------------------------------
+# SEASONS------------------------------------
 
-print_seasons_parser = print_subparsers.add_parser(
+seasons_parser = subparsers.add_parser(
     "seasons",
     description="Print a summary of all seasons.",
     parents=[common_parser],
 )
-print_seasons_parser.add_argument(
+seasons_parser.add_argument(
     "-t",
     "--top",
     metavar="COUNT",
@@ -163,23 +145,25 @@ def print_seasons(args: argparse.Namespace) -> None:
             print_err(str(ex))
 
 
-print_seasons_parser.set_defaults(print_func=print_seasons)
+seasons_parser.set_defaults(func=print_seasons)
 
-# PRINT TEAMS--------------------------------
+# TEAMS--------------------------------------
 
-print_teams_parser = print_subparsers.add_parser(
+teams_parser = subparsers.add_parser(
     "teams",
     description="Print team rankings for SEASON.",
     parents=[common_parser],
 )
-print_teams_parser.add_argument(
+teams_parser.add_argument(
     "season",
     metavar="SEASON",
     type=SeasonWeekType(),
+    nargs="?",
+    default="latest",
     action="store",
     help="Single season (e.g. 2018), a specific week within a season (e.g. 2014w10), or 'latest' to print the most recent rankings.",
 )
-print_teams_parser.add_argument(
+teams_parser.add_argument(
     "-r",
     "--rating",
     type=str.casefold,
@@ -188,7 +172,7 @@ print_teams_parser.add_argument(
     action="store",
     help="rating calculation to use for rankings",
 )
-print_teams_parser.add_argument(
+teams_parser.add_argument(
     "-t",
     "--top",
     metavar="COUNT",
@@ -209,23 +193,25 @@ def print_teams(args: argparse.Namespace) -> None:
             print_err(str(ex))
 
 
-print_teams_parser.set_defaults(print_func=print_teams)
+teams_parser.set_defaults(func=print_teams)
 
 # PRINT GAMES--------------------------------
 
-print_games_parser = print_subparsers.add_parser(
+games_parser = subparsers.add_parser(
     "games",
     description="Print game rankings for SEASON.",
     parents=[common_parser],
 )
-print_games_parser.add_argument(
+games_parser.add_argument(
     "season",
     metavar="SEASON",
     type=SeasonWeekType(),
+    nargs="?",
+    default="latest",
     action="store",
     help="Single season (e.g. 2018), a specific week within a season (e.g. 2014w10), or 'latest' to print the most recent rankings.",
 )
-print_games_parser.add_argument(
+games_parser.add_argument(
     "-r",
     "--rating",
     type=str.casefold,
@@ -234,7 +220,7 @@ print_games_parser.add_argument(
     action="store",
     help="rating calculation to use for rankings",
 )
-print_games_parser.add_argument(
+games_parser.add_argument(
     "-t",
     "--top",
     metavar="COUNT",
@@ -255,7 +241,7 @@ def print_games(args: argparse.Namespace) -> None:
             print_err(str(ex))
 
 
-print_games_parser.set_defaults(print_func=print_games)
+games_parser.set_defaults(func=print_games)
 
 # ENTRY POINT--------------------------------
 
