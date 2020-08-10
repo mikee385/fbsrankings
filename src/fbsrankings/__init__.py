1764d7db785c3144ca90869d1ba911100dda558f
Michael Pierce <mikee385@gmail.com>
2020-08-10 08:13:34 -0500

    Use pip-tools to manage requirements.txt

--- src/fbsrankings/__init__.py
+++ src/fbsrankings/__init__.py
@@ -3,5 +3,5 @@ Import college football teams and games from sportsreference.com and calculate r
 for the Division I Football Bowl Subdivision (FBS) teams.
 """
 
-__version__ = "4.5.0"
+__version__ = "4.6.0"
 __author__ = "Michael Stephen Pierce"
