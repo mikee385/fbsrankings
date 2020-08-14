b6ef91b88d81495988ad176aa9f02a4a517d4334
Michael Pierce <mikee385@gmail.com>
2020-08-14 13:04:26 -0500

    Update wheel dependency

--- src/fbsrankings/__init__.py
+++ src/fbsrankings/__init__.py
@@ -3,5 +3,5 @@ Import college football teams and games from sportsreference.com and calculate r
 for the Division I Football Bowl Subdivision (FBS) teams.
 """
 
-__version__ = "6.1.0"
+__version__ = "6.1.1"
 __author__ = "Michael Stephen Pierce"
