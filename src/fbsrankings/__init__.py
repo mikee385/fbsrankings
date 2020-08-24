f137e875d4ad2e7048b68deacfb27d7846cd7241
Michael Pierce <mikee385@gmail.com>
2020-08-24 15:21:30 -0500

    Update version number to 7.0.0

--- src/fbsrankings/__init__.py
+++ src/fbsrankings/__init__.py
@@ -3,5 +3,5 @@ Import college football teams and games from sportsreference.com and calculate r
 for the Division I Football Bowl Subdivision (FBS) teams.
 """
 
-__version__ = "7.0.0-dev.7"
+__version__ = "7.0.0"
 __author__ = "Michael Stephen Pierce"
