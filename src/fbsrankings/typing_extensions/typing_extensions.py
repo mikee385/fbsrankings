0b49db7413a78699f56c0cb97d07b3b68063319a
Michael Pierce <mikee385@gmail.com>
2024-02-21 11:19:17 -0600

    Add better comments to typing_extensions

--- src/fbsrankings/typing_extensions/typing_extensions.py
+++ src/fbsrankings/typing_extensions/typing_extensions.py
@@ -1,3 +1,8 @@
+# Utility types for typeshed
+#
+# Copied from typeshed on 2024-02-21:
+# https://github.com/python/typeshed/blob/main/stdlib/_typeshed/__init__.pyi
+
 from typing import Any
 from typing import TypeVar
 from typing import Union
@@ -5,21 +10,19 @@ from typing import Union
 from typing_extensions import TypeAlias
 from typing_extensions import Protocol
 
-
 # Comparison protocols
-# Copied from typeshed
 
-T = TypeVar("T", contravariant=True)
-
-
-class SupportsDunderLT(Protocol[T]):
-    def __lt__(self, other: T) -> bool:
-        raise NotImplementedError
+_T_contra = TypeVar("_T_contra", contravariant=True)
 
 
-class SupportsDunderGT(Protocol[T]):
-    def __gt__(self, other: T) -> bool:
-        raise NotImplementedError
+class SupportsDunderLT(Protocol[_T_contra]):
+    def __lt__(self, other: _T_contra) -> bool:
+        ...
+
+
+class SupportsDunderGT(Protocol[_T_contra]):
+    def __gt__(self, other: _T_contra) -> bool:
+        ...
 
 
 SupportsRichComparison: TypeAlias = Union[SupportsDunderLT[Any], SupportsDunderGT[Any]]
