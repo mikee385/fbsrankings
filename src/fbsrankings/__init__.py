06daba61c293c751e7ddfbabe03219745cf84f49
Michael Pierce <mikee385@gmail.com>
2025-03-22 20:03:58 -0600

    Increment version number to beta

--- src/fbsrankings/__init__.py
+++ src/fbsrankings/__init__.py
@@ -3,5 +3,5 @@ Import college football teams and games from sportsreference.com and calculate r
 for the Division I Football Bowl Subdivision (FBS) teams.
 """
 
-__version__ = "15.0.0"
+__version__ = "16.0.0-beta"
 __author__ = "Michael Stephen Pierce"
